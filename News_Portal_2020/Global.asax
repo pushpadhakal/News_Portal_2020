15cb9dbd4829394ebd062932f969c07bb6bc1aa2
pushpadhakal <47949651+pushpadhakal@users.noreply.github.com>
2020-08-24 00:19:06 -0400

    Add project files.

---
+++ News_Portal_2020/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="News_Portal_2020.MvcApplication" Language="C#" %>
---
+++ News_Portal_2020/Scripts/Africa/Africa.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(".AfricaConfidential").click(function () {
+        //window.location = "https://www.africa-confidential.com/news";
+        url = 'https://www.africa-confidential.com/news',
+            window.open(url, '_blank');
+    });
+
+
+
+});
